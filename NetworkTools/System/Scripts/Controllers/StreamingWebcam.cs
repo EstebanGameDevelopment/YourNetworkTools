da1fd260a5d07e46d39916a73b5c74c2fa4a0214
DESKTOP-K1COMOS\esteb <services@yourvrexperience.com>
2021-09-19 13:17:09 +0200

    Preprocessor constant added in order to avoid the request of camera permission in the manifest

--- NetworkTools/System/Scripts/Controllers/StreamingWebcam.cs
+++ NetworkTools/System/Scripts/Controllers/StreamingWebcam.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if ENABLE_WEBCAM_ACCESS
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -164,3 +165,4 @@ namespace YourNetworkingTools
 		}
 	}
 }
+#endif
